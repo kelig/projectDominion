e9cd0557ecd9aa514247522f53fdff505fc0bd2a
stijncoppens <stijn.coppens@student.howest.be>
2016-03-25 09:27:10 +0100

    Update script.js

--- html-css-javascript/programproject/programproject/grafische omgeving/assets/js/script.js
+++ html-css-javascript/programproject/programproject/grafische omgeving/assets/js/script.js
@@ -5,16 +5,72 @@
 
 
 // als je op een link drukt zal je naar de volgende pagina gaan
-var nextpage;
-nextpage=function(e){
+
+var startGame;
+startGame=function(e){
     e.preventDefault();
+    $("#firstpage").toggleClass('hide');
+    $("#secondpage").toggleClass('hide');
+    $("footer").toggleClass('hide');
+};
 
 
+var goToPlayers;
+goToPlayers=function(e){
+	e.preventDefault();
+	 $("#secondpage").toggleClass('hide');
+	 $('#thirdpageNewGame').toggleClass('hide')
+	 
+};
+
+var playerScreenFive;
+playerScreenFive=function(e){
+	e.preventDefault();
+	$("#thirdpageNewGame").toggleClass('hide');
+	 $('#fourthpage').toggleClass('hide')
+	 $('#extraPlayerOne').toggleClass('hide')
+	 $('#extraPlayerTwo').toggleClass('hide')
+	 $('#extraPlayerThree').toggleClass('hide')
+
+};
+var playerScreenFour;
+playerScreenFour=function(e){
+	e.preventDefault();
+	$("#thirdpageNewGame").toggleClass('hide');
+	 $('#fourthpage').toggleClass('hide')
+	 $('#extraPlayerOne').toggleClass('hide')
+	 $('#extraPlayerTwo').toggleClass('hide')
+
+};
+var playerScreenthree;
+playerScreenthree=function(e){
+	e.preventDefault();
+	$("#thirdpageNewGame").toggleClass('hide');
+	 $('#fourthpage').toggleClass('hide')
+	 $('#extraPlayerOne').toggleClass('hide')
+};
+
+var playerScreentwo;
+playerScreentwo=function(e){
+	e.preventDefault();
+	$("#thirdpageNewGame").toggleClass('hide');
+	 $('#fourthpage').toggleClass('hide')
+};
+
+
+var testJavascript=function(){
+	alert('this website is now online');
 };
 
 
 $(document).ready(function(){
-    $('a').on('click',nextpage);
-
-
+	testJavascript();
+	$('#play').on('click',startGame);
+	$('#newGame').on('click',goToPlayers);
+	$('#twoPlayers').on('click',playerScreentwo);
+	$('#threePlayers').on('click',playerScreenthree);
+	$('#fourPlayers').on('click',playerScreenFour);
+	$('#fivePlayers').on('click',playerScreenFive);
+	
 });
+
