5d4e6e80874f4651373416bbab6032a8dd48aa89
zegerde <zegerdeketelaere@gmail.com>
2016-05-10 21:44:57 +0200

    Update gameVisuals.js

--- html-css-javascript/programproject/graphicEnviroment/assets/js/gameVisuals.js
+++ html-css-javascript/programproject/graphicEnviroment/assets/js/gameVisuals.js
@@ -1,56 +1,99 @@
-/** Created by Zeger on 04/04/2016. */
+/**
+ * Created by Zeger on 04/04/2016.
+ */
 
 //Gekozen kaarten komen hier
 var Deck = ["Cellar","Market","Militia","Mine","Moat","Remodel","Smithy","Village","Woodcutter","Workshop"];
+var Coins = ["Estate","Copper","Duchy","Silver","Province","Gold","Curse"];
+var Hand = ["Estate","Estate","Copper","Copper","Copper"];
 
 
-//het deck wordt gemaakt en NORMAAL GEZIEN :p ook op de juiste plaats gezet, om de een of andere reden doet ie dat niet
+//de gekozen kaarten worden op de juiste plaats gezet
 var generateDeck = function(){
 
     for (var i = 0, len = Deck.length; i < len; i++) {
-        var html = '<img>';
 
         var src = 'images/' + Deck[i].toLowerCase() + '.jpg';
         console.log(src);
 
-        html += '<img alt="' + Deck[i] + '"  title="' + Deck[i] + '" src="' + src + '"/>';
-        html += '<span class="counter">10</span>';
-        html += '</img>';
-        $("#left-card.card").append(html);
+        var html = '<div class="card"><img alt="' + Deck[i] + '"  title="' + Deck[i] + '" src="' + src + '"/>';
+        html += '<span class="counter">10</span></class>';
+        html += '<span class="buy">+</span></class>';
+
+        //html += '</img>';
+        console.log(html);
+        $("#left-cards").append(html);
 
     }
-
-
 
 };
 
 
-//als je op een kaart klikt dat je kan kopen (links boven), dan komt die in de midden van het scherm. Deze code is nog niet af
+//de coins kaarten worden ingevoegd en  ook op de juiste plaats gezet
+var generateCoins = function(){
+
+    for (var i = 0, len = Coins.length; i < len; i++) {
+
+        var src = 'images/' + Coins[i].toLowerCase() + '.jpg';
+        console.log(src);
+
+        var html = '<div class="card"><img alt="' + Coins[i] + '"  title="' + Coins[i] + '" src="' + src + '"/>';
+        html += '<span class="counter">15</span></class>';
+        html += '<span class="buy">+</span></class>';
+
+        //html += '</img>';
+        console.log(html);
+        $("#right-cards").append(html);
+
+    }
+
+};
+
+var generateHand = function(){
+
+    for (var i = 0, len = Hand.length; i < len; i++) {
+
+        var src = 'images/' + Hand[i].toLowerCase() + '.jpg';
+        console.log(src);
+
+        var html = '<div class="card"><img alt="' + Hand[i] + '"  title="' + Hand[i] + '" src="' + src + '"/>';
+
+        //html += '</img>';
+        console.log(html);
+        $("#cards-hand").append(html);
+
+    }
+
+};
+
+//als je op een kaart klikt dat je kan kopen (links boven), dan komt die in de midden van het scherm, met een overlay er rond
 var showBigCard = function(){
     var CardUrl = $(this).attr('src');
     $('#cardBoxedAr').removeClass('hide');
+    $('#overlay').removeClass('hide');
 
     var Cardhtml = '<img src="'+CardUrl+'"/>';
-
-
-
     $('#showCard').append(Cardhtml);
 
 };
 //de grote kaart van het scherm doen
 var hideBigCard = function(){
     $('#cardBoxedAr').addClass('hide');
+    $('#overlay').addClass('hide');
+
     $('#showCard').empty();
-
 };
-
 
 
 
 $(document).ready(function(){
     console.log("Ok!");
     generateDeck();
+    generateCoins();
+    generateHand();
     $('#left-cards img').on('click',showBigCard);
-    $('').on('click', hideBigCard)
+    $('#cardBoxedAr').on('click', hideBigCard);
+    $('#overlay').on('click', hideBigCard);
+
 
 });
