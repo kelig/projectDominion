07606fbac500c4f855f84ee95d92232bcdc1affe
zegerde <zegerdeketelaere@gmail.com>
2016-05-18 16:07:20 +0200

    Add files via upload

---
+++ html-css-javascript/programproject/graphicEnviroment/assets/js/ajaxcallsGame.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Stijn on 12/05/2016.
+ */
+
+function buyCall(){
+    card=$(this).data('card');
+    var request = $.ajax({ cache: false,
+        url: "/DominionServer/DominionServlet",
+        data: { operation: 'buy',
+            card: card
+        }
+    });
+
+    request.done(function (data) {
+        console.log(card)
+    });
+    request.fail(function (jqXHR, textStatus) {
+        alert('an error occurred while communicating with the server, please reload the page and try again')
+    });
+}
+
+function handCall(){
+    card=$(this).data('card');
+    var request = $.ajax({ cache: false,
+        url: "/DominionServer/DominionServlet",
+        data: { operation: 'hand',
+            card: card
+        }
+    });
+
+    request.done(function (data) {
+        console.log(card)
+    });
+    request.fail(function (jqXHR, textStatus) {
+        alert('an error occurred while communicating with the server, please reload the page and try again')
+    });
+}
+
+function endTurn(){
+    var request = $.ajax({ cache: false,
+        url: "/DominionServer/DominionServlet",
+        data: { operation: 'endTurn',
+
+        }
+    });
+
+    request.done(function (data) {
+        console.log(card)
+    });
+    request.fail(function (jqXHR, textStatus) {
+        alert('an error occurred while communicating with the server, please reload the page and try again')
+    });
+
+
+}
+
+$(document).ready(function(){
+    $('.buy').on('click',buyCall);
+    $("#cards-hand").find(".card").on('click',handCall);
+    $('#endTurn').on('click',endTurn);
+});
