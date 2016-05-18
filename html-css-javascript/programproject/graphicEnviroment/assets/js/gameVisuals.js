815a2bd9fe9178f7d640e4039dafe7167e4c6a20
zegerde <zegerdeketelaere@gmail.com>
2016-05-18 16:05:45 +0200

    Update gameVisuals.js

--- html-css-javascript/programproject/graphicEnviroment/assets/js/gameVisuals.js
+++ html-css-javascript/programproject/graphicEnviroment/assets/js/gameVisuals.js
@@ -2,9 +2,6 @@
  * Created by Zeger on 04/04/2016.
  */
 
-
-var divGameView;
-$('#divGame').show();
 var coins = 0;
 var buys = 1;
 var actions = 1;
@@ -15,6 +12,8 @@ var actionButtons = function(){
     document.getElementById('actions').innerHTML=actions;
 
 };
+
+
 
 //Gekozen kaarten komen hier
 var Deck = ["Cellar","Market","Militia","Mine","Moat","Remodel","Smithy","Village","Woodcutter","Workshop"];
@@ -27,20 +26,16 @@ var generateDeck = function(){
 
     for (var i = 0, len = Deck.length; i < len; i++) {
 
-        var src = 'images/' + Deck[i].toLowerCase() + '.jpg';
-        console.log(src);
+        var src = 'images/' + Deck[i] + '.jpg';
 
         var html = '<div class="card"><img alt="' + Deck[i] + '"  title="' + Deck[i] + '" src="' + src + '"/>';
-        html += '<span class="counter">10</span></class>';
-        html += '<span class="buy">+</span></class>';
+        html += '<span class="counter">10</span>';
+        html += '<span class="buy" data-card="'+Deck[i]+'">+</span>';
 
-        //html += '</img>';
-        console.log(html);
         $("#left-cards").append(html);
-
     }
-
 };
+
 
 
 //de coins kaarten worden ingevoegd en  ook op de juiste plaats gezet
@@ -48,42 +43,52 @@ var generateCoins = function(){
 
     for (var i = 0, len = Coins.length; i < len; i++) {
 
-        var src = 'images/' + Coins[i].toLowerCase() + '.jpg';
-        console.log(src);
+        var src = 'images/' + Coins[i] + '.jpg';
 
         var html = '<div class="card"><img alt="' + Coins[i] + '"  title="' + Coins[i] + '" src="' + src + '"/>';
-        html += '<span class="counter">15</span></class>';
-        html += '<span class="buy">+</span></class>';
+        html += '<span class="counter">15</span>';
 
-        //html += '</img>';
-        console.log(html);
+        html += '<span class="buy" data-card="' + Coins[i] + '">+</span>';
+
         $("#right-cards").append(html);
     }
 
 };
 
+
 var generateHand = function(){
 
     for (var i = 0, len = Hand.length; i < len; i++) {
 
-        var src = 'images/' + Hand[i].toLowerCase() + '.jpg';
-        console.log(src);
+        var src = 'images/' + Hand[i] + '.jpg';
 
-        var html = '<div class="card"><img alt="' + Hand[i] + '"  title="' + Hand[i] + '" src="' + src + '"/>';
+        var html = '<div class="card" data-card="' + Hand[i] + '">';
+        html+='<img alt="' + Hand[i] + '"  title="' + Hand[i] + '" src="' + src + '"/></div>';
 
-        //html += '</img>';
-        console.log(html);
         $("#cards-hand").append(html);
-
     }
-
 };
+
+var cardToPlayfield = function() {
+    var CardUrl = $(this).attr('src');
+
+    var Cardhtml = '<div class="card"><img src="'+CardUrl+'"/>';
+
+    $("#playfield .card").css('left', '+=40%');
+
+    $( Cardhtml).appendTo( $( ".playfield" ) );
+
+    $(this).closest('.card').remove();
+};
+
+
 
 //als je op een kaart klikt dat je kan kopen (links boven), dan komt die in de midden van het scherm, met een overlay er rond
 var showBigCard = function(){
     var CardUrl = $(this).attr('src');
     $('#cardBoxedAr').removeClass('hide');
     $('#overlay').removeClass('hide');
+    $('#overlay').css({"background-color" : "#000000 ", "opacity":"0.5"});
 
     var Cardhtml = '<img src="'+CardUrl+'"/>';
     $('#showCard').append(Cardhtml);
@@ -98,17 +103,37 @@ var hideBigCard = function(){
 };
 
 
+//tutorial op het scherm tonen
+var showTutorialOverlay = function(){
 
+    $('#overlayTutorial').removeClass('hide');
+    $('#overlay').removeClass('hide').css({"background-color" : "#F0F0F0 ", "opacity":"0.2"});
+
+    console.log("hallo");
+
+};
+//tutorial van het scherm
+var hideTutorialOverlay = function(){
+
+    $('#overlayTutorial').addClass('hide');
+    $('#overlay').addClass('hide');
+
+    console.log("hallo2");
+};
 
 $(document).ready(function(){
     console.log("Ok!");
     generateDeck();
     generateCoins();
     generateHand();
+    $('#cards-hand img').on('click',cardToPlayfield);
     $('#left-cards img').on('click',showBigCard);
     $('#cardBoxedAr').on('click', hideBigCard);
-    $('#overlay').on('click', hideBigCard);
-    actionButtons();
+    $('#overlay').on('click', hideBigCard );
+
+    $('#tutorial').on('click', showTutorialOverlay);
+    $('#overlay').on('click', hideTutorialOverlay);
+
 
 });
 //Disable f5 & f6 key & drag
